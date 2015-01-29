84ade32d46a61b452c67c9116216cd4770e19422
rstudio <rstudio@example.com>
2015-01-29 03:02:31 -0800

    making everything into functions

--- code/libraries.R
+++ code/libraries.R
@@ -1,49 +1,6 @@
 ##  ---- libraries
 
-
-# libraries need to be in a .R file so packrat can find them
-# to bundle them in with the repository 
-
-# set repository to download packages from 
-r <- getOption("repos")
-r["CRAN"] <- "http://cran.rstudio.com/"
-options(repos = r)
-rm(r)
-
-# function to download packages if they are not 
-# available locally
-
-ipak <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) 
-    install.packages(new.pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-}
-
-# The list of packages needed by this repository
-pkgs <- c('knitr', 'ggplot2', 'reshape2', 'scales', 'MCMCpack', 'BEST', 'bcp', 'RColorBrewer', 'devtools', 'gridExtra', 'httr', 'memoise', 'whisker')
-ipak(pkgs)
-
-# assuming that devtools is now installed...
-ipak_github <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) 
-    devtools::install_github(new.pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-}
-
-github_pkgs <- c("rmarkdown")
-lapply(github_pkgs, ipak_github)
-
-# this is for reshape2, oddly the repo name (reshape) is different from the 
-# package name (reshape2)...
-ipak_github_reshape <- function(pkg,pkg2){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) 
-    devtools::install_github(new.pkg, dependencies = TRUE)
-  sapply(pkg2, require, character.only = TRUE)
-}
-ipak_github_reshape("reshape", "reshape2")
+# just a list of libraries used here
 
 require(knitr)
 require(ggplot2)
@@ -54,7 +11,8 @@ require(BEST)
 require(bcp)
 require(RColorBrewer)
 require(gridExtra)
-
+require(analogue)
+require(rioja)
 require(devtools)
 require(httr)
 require(memoise)
