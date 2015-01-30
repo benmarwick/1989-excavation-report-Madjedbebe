252f2e98ad265e6a4f7f32e7bcb1199926188ecd
rstudio <rstudio@example.com>
2015-01-29 18:17:27 -0800

    working on fig 10 raw materials

--- code/libraries.R
+++ code/libraries.R
@@ -2,20 +2,20 @@
 
 # just a list of libraries used here
 
-require(knitr)
-require(ggplot2)
-require(reshape2)
-require(scales)
-require(MCMCpack)
-require(BEST)
-require(bcp)
-require(RColorBrewer)
-require(gridExtra)
-require(analogue)
-require(rioja)
-require(devtools)
-require(httr)
-require(memoise)
-require(rmarkdown)
-require(whisker)
+knitr
+ggplot2
+reshape2
+scales
+MCMCpack
+BEST
+bcp
+RColorBrewer
+gridExtra
+analogue
+rioja
+devtools
+httr
+memoise
+rmarkdown
+whisker
 
